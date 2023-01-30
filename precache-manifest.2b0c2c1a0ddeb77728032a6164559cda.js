4661f197b7dccbf476961dc0fa1f34b78cd2d438
KetulThakor1 <kleapfrogdev@gmail.com>
2023-01-30 16:08:33 +0530

    Updates

---
+++ precache-manifest.2b0c2c1a0ddeb77728032a6164559cda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579fb10386a9e80b4ac8a42b15c72762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbff4f5194e02a66ea27",
+    "url": "/static/css/2.ec512844.chunk.css"
+  },
+  {
+    "revision": "aaa047066604d641c47c",
+    "url": "/static/css/main.7ce789c9.chunk.css"
+  },
+  {
+    "revision": "bbff4f5194e02a66ea27",
+    "url": "/static/js/2.61a78a06.chunk.js"
+  },
+  {
+    "revision": "0bc468039aa47f0d3a181fe7ee5a035a",
+    "url": "/static/js/2.61a78a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02904309f3ae8506fa42",
+    "url": "/static/js/3.6b3cc30e.chunk.js"
+  },
+  {
+    "revision": "2ee4a329ded82e8e12215f331bb46922",
+    "url": "/static/js/3.6b3cc30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cbba9e15308aecd90c",
+    "url": "/static/js/4.a00ef9aa.chunk.js"
+  },
+  {
+    "revision": "aaa047066604d641c47c",
+    "url": "/static/js/main.46ea7489.chunk.js"
+  },
+  {
+    "revision": "19da01f3197cc0bd3594",
+    "url": "/static/js/runtime-main.f5077313.js"
+  }
+]);
