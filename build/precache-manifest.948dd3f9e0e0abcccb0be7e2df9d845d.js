f4aa43629fc69523ec1870376cc6fa7b76d18901
MaulikDoriya-at-360607551768 <maulik.doriya@creditmountain.co>
2023-08-04 17:25:46 +0530

    latest build

---
+++ build/precache-manifest.948dd3f9e0e0abcccb0be7e2df9d845d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c6d5bbc9b1b177ee49e2910a20ae94",
+    "url": "/UrbanMiner_Admin/index.html"
+  },
+  {
+    "revision": "abb73eebfb966ae0660c",
+    "url": "/UrbanMiner_Admin/static/css/2.12770553.chunk.css"
+  },
+  {
+    "revision": "10dbbd5619f075937141",
+    "url": "/UrbanMiner_Admin/static/css/main.75bca7c5.chunk.css"
+  },
+  {
+    "revision": "abb73eebfb966ae0660c",
+    "url": "/UrbanMiner_Admin/static/js/2.ffeb501f.chunk.js"
+  },
+  {
+    "revision": "d088eba47f8645ee11979448c3fe04f2",
+    "url": "/UrbanMiner_Admin/static/js/2.ffeb501f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c196dc74b9b478fc487",
+    "url": "/UrbanMiner_Admin/static/js/3.5ef10954.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/UrbanMiner_Admin/static/js/3.5ef10954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dbbd5619f075937141",
+    "url": "/UrbanMiner_Admin/static/js/main.49c582a3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/UrbanMiner_Admin/static/js/main.49c582a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d540af843f7f8af17de",
+    "url": "/UrbanMiner_Admin/static/js/runtime-main.48896057.js"
+  },
+  {
+    "revision": "6906ca8ee5e2141d19fda9fbf91ccfc9",
+    "url": "/UrbanMiner_Admin/static/media/urbanminer.6906ca8e.png"
+  }
+]);
