9b81d60c8a41c6c2eb3cb481bdda63f04cd9cbaa
“KetulThakor1” <kleapfrogdev@gmail.com>
2024-04-05 17:20:54 +0530

    add material category feature added

--- src/routes/MaterialList/index.js
+++ src/routes/MaterialList/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ArrowForwardIos } from '@material-ui/icons';
-import { ArrowDropDown } from '@mui/icons-material';
+import { ArrowDropDown, Delete } from '@mui/icons-material';
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { useFormik } from 'formik';
@@ -15,7 +15,7 @@ import {
     getAllMaterialList,
     updateMaterialItem
 } from '../../Firebase/materialList/index';
-import { onValue, ref } from 'firebase/database';
+import { onValue, ref, remove, set } from 'firebase/database';
 import { formateData } from 'util/formateData';
 import * as Config from '../../configs/index';
 
@@ -134,9 +134,84 @@ export default function MaterialList() {
         }
     }
 
+    const [category, setCategory] = useState('');
+    const [categoryList, setCategoryList] = useState([]);
+
+    useEffect(() => {
+        getAllCategoryList();
+    }, []);
+
+    const getAllCategoryList = async () => {
+        const materialRef = ref(Config.database, '/ADMIN/MATERIAL_CATEGORY_LIST');
+
+        await onValue(materialRef, (snapshot) => {
+            const results = snapshot.val();
+
+            const data = formateData(results);
+
+            setCategoryList(data);
+        });
+    };
+
+    const handleAddCategory = async () => {
+        if (category) {
+            const id = uuid().slice(0, 8);
+
+            const materialRef = ref(Config.database, `/ADMIN/MATERIAL_CATEGORY_LIST/${id}`);
+
+            await set(materialRef, {
+                id: id,
+                category: category
+            });
+            setCategory('');
+            notify('Category Added Successfully!', 1);
+        }
+    };
+
+    const handleDeleteCategory = async (id) => {
+        const materialRef = ref(Config.database, `/ADMIN/MATERIAL_CATEGORY_LIST/${id}`);
+
+        await remove(materialRef);
+
+        notify('Category Deleted Successfully!', 1);
+    };
+
     return (
         <div className='flex flex-1 flex-col gap-10'>
             {/*  */}
+
+            <div className='flex items-center gap-8'>
+                <input
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    placeholder='Enter Category Name'
+                    className='h-12 bg-white border rounded-lg outline-none px-4 font-bold  
+                    placeholder:text-gray-300 text-black placeholder:font-bold'
+                />
+                <div
+                    onClick={handleAddCategory}
+                    className='font-bold text-lg text-blue-700 cursor-pointer'
+                >
+                    ADD
+                </div>
+            </div>
+            <div className='flex flex-col gap-2 w-1/2'>
+                {categoryList?.map((item) => {
+                    return (
+                        <div className='flex items-center gap-8 bg-slate-200 justify-between p-2 rounded-md'>
+                            <div className='font-bold text-lg text-black cursor-pointer'>
+                                {item.category}
+                            </div>
+                            <div
+                                className='font-bold text-lg text-red-500 cursor-pointer'
+                                onClick={() => handleDeleteCategory(item.id)}
+                            >
+                                <Delete />
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
             <div className='flex items-center gap-10 font-bold text-lg text-blue-700 '>
                 <div
                     className='cursor-pointer'
